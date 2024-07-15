cdddaa88d80e909b2273bd666fe81f8f68d1e261
Louis Sun <2207740560@qq.com>
2024-07-16 03:32:43 +0800

    3 points scheme

--- Example/FDMAdvection3points1D.m
+++ Example/FDMAdvection3points1D.m
@@ -7,11 +7,11 @@ delta_t = 0.01;            % Time step size
 delta_x = 0.01;            % Space step size
 x_end = 1.0;               % End point of the spatial domain
 t_end = 1.0;               % End point of the time domain
-scheme = 'FW';             % Choose scheme ('LF', 'LW', 'CD', 'BW', 'FW')
+scheme = 'BW';             % Choose scheme ('LF', 'LW', 'CD', 'BW', 'FW')
 
 % Boundary and initial conditions
 fai = @(t) sin(2 * pi * t); % Boundary condition at x=0
-g = @(x) sin(2 * pi * x);   % Initial condition at t=0
+g = @(x) 0;   % Initial condition at t=0
 
 % Solve the PDE
 u = FDM3points(a, delta_t, delta_x, x_end, t_end, scheme, fai, g)';
