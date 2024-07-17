3134944e4d95e9fd062702c6335e9fc2974148a5
Louis Sun <2207740560@qq.com>
2024-07-17 09:25:30 +0800

    Box scheme

---
+++ Example/FDMAdvectionBox1D.m
@@ -0,0 +1,42 @@
+clear;
+addpath('../FDMscheme/');
+
+% Advection speed
+a = 1.0;
+
+% Time step size
+delta_t = 0.01;
+
+% Space step size
+delta_x = 0.01;
+
+% Spatial domain
+x_start = 0.0;
+x_end = 1.0;
+
+% Time domain
+t_start = 0.0;
+t_end = 1.0;
+
+% Boundary and initial conditions
+fai = @(t) sin(2 * pi * t); % Boundary condition at x=0
+g = @(x) 0;   % Initial condition at t=0
+
+% Solve the PDE
+u = Boxscheme(a, delta_t, delta_x, x_start, x_end, t_start, t_end, fai, g);
+
+% Plot the result
+x = linspace(x_start, x_end, floor((x_end - x_start) / delta_x) + 1);
+t = linspace(t_start, t_end, floor((t_end - t_start) / delta_t) + 1);
+[T, X] = meshgrid(t, x);
+
+% Transpose u to align with T and X
+u = u';
+
+% Plot the surface
+figure;
+surf(X, T, u);
+xlabel('x');
+ylabel('t');
+zlabel('u(x,t)');
+title('Solution of the advection equation using the Box scheme method');
---
+++ FDMscheme/Boxscheme.m
@@ -0,0 +1,58 @@
+function u = Boxscheme(a, delta_t, delta_x, x_start, x_end, t_start, t_end, fai, g)
+    % Boxscheme - Solves the PDE u_t + a u_x = 0 using the Box scheme method
+    %
+    % Syntax:
+    %   u = Boxscheme(a, delta_t, delta_x, x_start, x_end, t_start, t_end, fai, g)
+    %
+    % Inputs:
+    %   a         - Advection speed
+    %   delta_t   - Time step size
+    %   delta_x   - Space step size
+    %   x_start   - Start point of the spatial domain
+    %   x_end     - End point of the spatial domain
+    %   t_start   - Start point of the time domain
+    %   t_end     - End point of the time domain
+    %   fai       - Function handle for boundary condition u(t,0)
+    %   g         - Function handle for initial condition u(0,x)
+    %
+    % Outputs:
+    %   u         - Solution matrix where each row is the state vector at a time step
+    %
+    %   Written by Qi Sun, July 2024.
+
+    % Check if fai(t_start) equals g(x_start)
+    if fai(t_start) ~= g(x_start)
+        error('Boundary condition fai(t_start) must equal initial condition g(x_start).');
+    end
+
+    % Calculate number of steps
+    num_t_points = floor((t_end - t_start) / delta_t) + 1;
+    num_x_points = floor((x_end - x_start) / delta_x) + 1;
+
+    % Calculate lambda and nu
+    lambda = delta_t / delta_x;
+    nu = a * lambda;
+
+    % Initialize the spatial domain and initial condition
+    x = linspace(x_start, x_end, num_x_points);
+    t = linspace(t_start, t_end, num_t_points);
+    u = zeros(num_t_points, num_x_points);
+    u(1, :) = g(x);
+
+    % Time evolution using Box scheme
+    for n = 1:num_t_points-1
+        % Temporary array to store intermediate step values
+        u_next = zeros(1, num_x_points);
+
+        % Apply boundary condition at x=0
+        u_next(1) = fai(t(n+1));
+
+        % Apply the Box scheme for internal points
+        for j = 2:num_x_points
+            u_next(j) = (u(n, j-1) + ((1 - nu)/(1 + nu)) * (u(n, j) - u_next(j-1)));
+        end
+
+        % Update the solution
+        u(n+1, :) = u_next;
+    end
+end
